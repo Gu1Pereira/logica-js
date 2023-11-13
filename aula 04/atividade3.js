e5cb8f87463af998e0a2b71782b417ae13fb1eea
Gu1Pereira <143458246+Gu1Pereira@users.noreply.github.com>
2023-11-13 23:41:31 +0000

    Laço Para Até

---
+++ aula 04/atividade3.js
@@ -0,0 +1,18 @@
+// Laço Para Até
+
+for (let i=1; i<=10 ; i++){
+    console.log(i)
+}
+for (let i = 0; i <= 5; i++){
+    console.log("Botafogo campeão " + i)
+}
+
+// var é no sistema inteiro enquanto let é so dentro das {}
+
+/*
+ let i = 1 // inicialização 
+ while (i <= 1){ //  condição){
+     console.log("Botafogo campeão")
+     i ++ // incremento
+ }
+ */
