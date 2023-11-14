ce003ac418a92aa3c18cfe82a311b122a0875b1c
Gu1Pereira <143458246+Gu1Pereira@users.noreply.github.com>
2023-11-14 01:16:53 +0000

    exercicio 1

---
+++ aula 04/exercicio1.js
@@ -0,0 +1,23 @@
+/*Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois modifique o programa para que ele mostre os números um ao lado do outro.*/
+
+
+let i = 1
+let numeros
+while(i<=20){
+    numero = numero + " " + i
+    // numero += " " + i
+}
+console.log(numeros)
+
+
+
+/*
+for (let i=1; i<=20 ; i++){
+    console.log(i)
+}
+let number = " "
+for (let i = 1; i <= 20; i++){
+    number += i + " "
+}
+console.log(number)
+*/
