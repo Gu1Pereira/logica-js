3be48c9d7bde2b65778c0080af3d308fcd4e8abf
Gu1Pereira <143458246+Gu1Pereira@users.noreply.github.com>
2023-11-14 22:40:42 +0000

    exercicio 3

--- aula 04/exercicio1.js
+++ aula 04/exercicio1.js
@@ -1,17 +1,6 @@
 /*Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois modifique o programa para que ele mostre os números um ao lado do outro.*/
 
 
-let i = 1
-let numeros
-while(i<=20){
-    numero = numero + " " + i
-    // numero += " " + i
-}
-console.log(numeros)
-
-
-
-/*
 for (let i=1; i<=20 ; i++){
     console.log(i)
 }
@@ -20,4 +9,3 @@ for (let i = 1; i <= 20; i++){
     number += i + " "
 }
 console.log(number)
-*/
--- aula 04/exercicio2.js
+++ aula 04/exercicio2.js
@@ -1,3 +1,20 @@
 /*
 Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
 */
+
+for (let numero = 1; numero <= 50; numero++) {
+    if (numero % 2 !== 0) {
+        console.log(numero);
+    }
+}
+
+/*
+let i = 1 
+While( <= 50){
+    if(i%2 !=0){
+        console.log(i)
+    }
+    i++
+}
+
+*/
