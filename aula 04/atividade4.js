f16cf20d4484ce4ca463ead2f42b9cea41ce8815
Gu1Pereira <143458246+Gu1Pereira@users.noreply.github.com>
2023-11-14 00:48:30 +0000

    laço de repetição com array

---
+++ aula 04/atividade4.js
@@ -0,0 +1,13 @@
+let frutas = ["🍇", "🍊","🍎","🍓","🍐"]
+frutas.push("🍉")
+frutas.push("🍍")
+/*
+for (let i = 0; i < frutas.length; i++) {
+  console.log(frutas[i]);
+}
+*/
+let i = 0
+while(i < frutas.length){
+    console.log(frutas[i])
+    i++
+}
