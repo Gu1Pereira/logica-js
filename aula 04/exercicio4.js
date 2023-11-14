5579985a1c5b861565963e258b69d8426e8f7120
Gu1Pereira <143458246+Gu1Pereira@users.noreply.github.com>
2023-11-14 22:59:27 +0000

    exercicio 4

--- aula 04/exercicio4.js
+++ aula 04/exercicio4.js
@@ -1,1 +1,12 @@
 //Altere o programa anterior para mostrar no final a soma dos números.
+
+let inicio = 7 
+let fim = 25
+let soma = 0
+
+while(inicio < fim-1){
+    inicio++
+    console.log(inicio)
+    soma += inicio    // soma = soma + inicio
+   }
+   console.log(soma)
